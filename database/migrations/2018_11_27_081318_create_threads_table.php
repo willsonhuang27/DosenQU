723bfc5203ba2ecf70089e39d193474a38f06634
willsonhuang27 <45695671+willsonhuang27@users.noreply.github.com>
2018-12-08 10:53:07 +0700

    Add migration file
    Untuk menambahkan table di database

---
+++ database/migrations/2018_11_27_081318_create_threads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('stsrc');
+            $table->increments('thread_id');
+            $table->string('forum_id');
+            $table->string('content');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
