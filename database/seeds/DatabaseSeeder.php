0ec55e8c07dafcb5b48fb4e356501c15c5443217
willsonhuang27 <45695671+willsonhuang27@users.noreply.github.com>
2018-12-08 09:04:17 +0700

    Add EmptyProject
    As template for DosenQU but without vendor folder

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {   //buat jalanin seeder
+        // $this->call(UsersTableSeeder::class);
+        $this->call(UserSeeder::class);
+    }
+}
