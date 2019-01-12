56884a80112c5c3ecc9035564752d026ffca75e1
willsonhuang27 <45695671+willsonhuang27@users.noreply.github.com>
2019-01-12 12:35:45 +0700

    Delete dashboard.blade.php

--- resources/views/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>This is Dashboard</h1>
-
-{{--<form action="{{url('/logout')}}" method="POST">--}}
-    {{--{{csrf_field()}}--}}
-    {{--<input type="submit" placeholder="Logout">--}}
-{{--</form>--}}
